7dab0f55adcb6a1f366a8e30fba63c4f05f5f9d1
julialee0318 <85609232+julialee0318@users.noreply.github.com>
2023-05-03 12:47:00 +0800

    Update demo_store_testing.py

--- demo_store_testing.py
+++ demo_store_testing.py
@@ -2,21 +2,13 @@ from bs4 import BeautifulSoup
 
 # Load the HTML file
 # r is read mode
-with open('demo_store.html', 'r') as f:
+with open('drinks&co.html', 'r') as f:
     html = f.read()
 
 # Parse the HTML using BeautifulSoup
 soup = BeautifulSoup(html, 'html.parser')
 
 # Finding input elements in the URL
-# With types 'text' or 'email' or 'tel'
-input_elements = soup.find_all('input',{'type':['text','email','tel']})
-
-# Print the number of input elements
-# Print the extract attributes
-print(len(input_elements))
-
-for input_element in input_elements:
-    print(input_element.attrs)
-
+# Only include types that will require user input
+input_elements = soup.find_all('input', {'type': ['text', 'email', 'tel', 'date', 'datetime-local', 'number', 'month', 'time']})
 
